ee53408959f56ed0135efb84f6a8c3a4d503f065
MalikAza <MalikAza@users.noreply.github.com>
2022-12-13 19:33:52 +0100

    serverinfo cmd in core

--- core.py
+++ core.py
@@ -3,10 +3,14 @@ from discord.ext import commands
 import os
 import sys
 import datetime
+import time
 import aiohttp
+
 from dotenv import load_dotenv
 load_dotenv()
 POWI_GUILD_ID = os.getenv('POWI_GUILD_ID')
+
+from .cogs.utils.chat_formatting import humanize_timedelta
 
 class ConfirmLeaveServer(discord.ui.View):
     def __init__(self, author, server):
@@ -315,6 +319,99 @@ class Core(commands.Cog):
         else:
             await ctx.bot.change_presence(activity=None, status=status)
         await ctx.send("Done.")
+
+    @commands.command()
+    async def serverinfo(self, ctx):
+        """Show server informations."""
+        server = ctx.guild
+        created_at = "Créé le {date_and_time}. C'étais {relative_time} !".format(
+            date_and_time=discord.utils.format_dt(server.created_at),
+            relative_time=discord.utils.format_dt(server.created_at, "R")
+        )
+        online = len([m.status for m in server.members if m.status != discord.Status.offline])
+        total_users = server.member_count
+        member_msg = f"Utilisateurs en ligne: **{online}/{total_users}**\n"
+
+        text_channels = len(server.text_channels)
+        voice_channels = len(server.voice_channels)
+        
+        online_stats = {
+            "Humains : ": lambda x: not x.bot,
+            "\nBots : ": lambda x: x.bot,
+            "\N{LARGE GREEN CIRCLE}": lambda x: x.status is discord.Status.online,
+            "\N{LARGE ORANGE CIRCLE}": lambda x: x.status is discord.Status.idle,
+            "\N{LARGE RED CIRCLE}": lambda x: x.status is discord.Status.do_not_disturb,
+            "\N{MEDIUM WHITE CIRCLE}\N{VARIATION SELECTOR-16}": lambda x: x.status is discord.Status.offline,
+            "\N{LARGE PURPLE CIRCLE}": lambda x: any(a.type is discord.ActivityType.streaming for a in x.activities),
+            "\N{MOBILE PHONE}": lambda x: x.is_on_mobile()
+        }
+        count = 1
+        for emoji, value in online_stats.items():
+            try:
+                num = len([m for m in server.members if value(m)])
+            except Exception as error:
+                print(error)
+                continue
+            else:
+                member_msg += f"{emoji} **{num}** " + ("\n" if count%2 == 0 else "")
+            count += 1
+
+        verif = {
+            "none": "0 - Aucune",
+            "low": "1 - Faible",
+            "medium": "2 - Moyenne",
+            "high": "3 - Haute",
+            "hightest": "4 - La plus haute"
+        }
+
+        joined_on = "{bot_name} a rejoint ce serveur le {bot_join}. Il y a plus de {since_join} jours !".format(
+            bot_name = ctx.bot.user.name,
+            bot_join = server.me.joined_at.strftime("%d %b %Y %H:%M:%S"),
+            since_join = (ctx.message.created_at - server.me.joined_at).days
+        )
+
+        embed = discord.Embed(description=(f"{server.description}\n\n" if server.description else "") + created_at,
+                color=server.me.color)
+
+        embed.set_author(name=server.name,
+        icon_url="https://cdn.discordapp.com/emojis/457879292152381443.png" if "VERIFIED" in server.features
+        else "https://cdn.discordapp.com/emojis/508929941610430464.png" if "PARTNERED" in server.features
+        else None)
+
+        if server.icon:
+            embed.set_thumbnail(url=server.icon)
+
+        embed.add_field(name="Membres :", value=member_msg)
+
+        embed.add_field(name="Salons :",
+                        value="\N{SPEECH BALLOON} Text : {text}\n"
+                        "\N{SPEAKER WITH THREE SOUND WAVES} Vocal : {voice}\n"
+                            .format(text=f"**{text_channels}**",
+                                    voice=f"**{voice_channels}**"))
+
+        embed.add_field(name="Utilitaire :",
+                        value="Propriétaire : {owner}\nNiveau verif. : {verif}\nServer ID : {id}"
+                            .format(owner=f"**{server.owner}**",
+                                    verif=f"**{verif[str(server.verification_level)]}**",
+                                    id=f"**{server.id}**"),
+                        inline=False)
+
+        embed.add_field(name="Divers :",
+                        value="Salon AFK : {afk_chan}\nPériode d'inactivité : {afk_timeout}\nEmojis : {emoji_count}\nRôles : {role_count}"
+                        .format(
+                            afk_chan=f"**{str(server.afk_channel) if server.afk_channel else 'Aucun'}**",
+                            afk_timeout=f"**{humanize_timedelta(timedelta=datetime.timedelta(seconds=server.afk_timeout))}**",
+                            emoji_count=f"**{len(server.emojis)}**",
+                            role_count=f"**{len(server.roles)}**"
+                        ),
+                        inline=False)
+
+        if server.splash:
+            embed.set_image(url=server.splash.replace(format="png"))
+        
+        embed.set_footer(text=joined_on)
+
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Core(bot))
