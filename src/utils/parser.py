54f14c15ff86cb8438211cbc64e6c349d0314335
Dakota <dakota742009@gmail.com>
2025-02-15 17:21:14 +0100

    feat: utils SetParser

---
+++ src/utils/parser.py
@@ -0,0 +1,21 @@
+from typing import Literal
+
+class SetParser:
+    sum: int
+    operation: Literal['withdraw', 'deposit', 'set']
+
+    def __init__(self, argument):
+        allowed = ("+", "-")
+        self.sum = int(argument)
+        if argument and argument[0] in allowed:
+            if self.sum < 0:
+                self.operation = "withdraw"
+            elif self.sum > 0:
+                self.operation = "deposit"
+            else:
+                raise RuntimeError
+            self.sum = abs(self.sum)
+        elif argument.isdigit():
+            self.operation = "set"
+        else:
+            raise RuntimeError
