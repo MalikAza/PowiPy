a6f3a71ab4b4f3b2ac21e6eee40033fac4287901
Dakota <dakota742009@gmail.com>
2025-02-28 14:17:02 +0100

    migrate: prefix changed

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from src import Client
 
 def main():
     token = os.getenv('TOKEN')
-    bot = Client(command_prefix=";")
+    bot = Client(command_prefix="!")
     bot.run(token, log_handler=None)
 
 if __name__ == '__main__':
